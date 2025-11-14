d8cb35a4fc6aad3d1a9714833eb77b9e002a2c1d
Toneri <104565824+toneri404@users.noreply.github.com>
2025-11-14 11:35:25 +0600

    Add files via upload

--- db.js
+++ db.js
@@ -1,17 +1,14 @@
-// db.js
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "redstone_hub",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
 });
-
-// 👇 Add this line
-export default pool;
